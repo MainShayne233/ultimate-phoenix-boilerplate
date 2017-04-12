a9194b8e12142b57d893f992ea878ac5de8bf149
MainShayne233 <shaynetremblay@hotmail.com>
2017-04-11 21:27:51 -0400

    generates config/prod.secret.exs

--- lib/mix/tasks/app/setup.ex
+++ lib/mix/tasks/app/setup.ex
@@ -6,6 +6,8 @@ defmodule Mix.Tasks.App.Setup do
     git_init()
     IO.puts "Fetching dependencies"
     Mix.Tasks.App.Rename.run([name, otp])
+    IO.puts "Create config/prod.secret.exs"
+    create_prod_secret_config(name, otp)
     IO.puts "Installing npm packages"
     node_init()
     IO.puts """
@@ -31,7 +33,7 @@ defmodule Mix.Tasks.App.Setup do
     """
   end
 
-  def git_init do
+  defp git_init do
     [
       "rm -rf .git",
       "git init",
@@ -41,10 +43,33 @@ defmodule Mix.Tasks.App.Setup do
     |> Enum.each(&Mix.Shell.IO.cmd/1)
   end
 
-  def node_init do
+  defp node_init do
     File.cd!("assets")
     Mix.Shell.IO.cmd("npm i")
     File.cd!("..")
+  end
+
+  defp create_prod_secret_config(name, otp) do
+    file = """
+    use Mix.Config
+
+    config :#{otp}, #{name}.Web.Endpoint,
+      secret_key_base: "#{new_secret()}"
+    
+    config :#{otp}, #{name}.Repo,
+      adapter: Ecto.Adapters.Postgres,
+      username: "postgres",
+      password: "postgres",
+      database: "#{otp}_prod",
+      pool_size: 15
+    """
+  end
+
+  def new_secret do
+    64
+    |> :crypto.strong_rand_bytes
+    |> Base.encode64 
+    |> binary_part(0, 64)
   end
 
 end
