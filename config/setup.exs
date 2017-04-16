da324c624f6f9c2eb8e14f06a7c9f7e0121a1333
MainShayne233 <shaynetremblay@hotmail.com>
2017-04-16 14:29:03 -0400

    complets elm procedure

--- config/setup.exs
+++ config/setup.exs
@@ -2,14 +2,14 @@ use Mix.Config
 
 # Sample config for React frontend
 
-#config :ultimate_phoenix_boilerplate, Mix.Tasks.App.Setup,
-#  name: "NewApp",
-#  otp: "new_app",
-#  frontend: "react"
-
-# Sample config for Elm frontend
-
 config :ultimate_phoenix_boilerplate, Mix.Tasks.App.Setup,
   name: "NewApp",
   otp: "new_app",
-  frontend: "elm"
+  frontend: "react"
+
+# Sample config for Elm frontend
+
+# config :ultimate_phoenix_boilerplate, Mix.Tasks.App.Setup,
+#   name: "NewApp",
+#   otp: "new_app",
+#   frontend: "elm"
