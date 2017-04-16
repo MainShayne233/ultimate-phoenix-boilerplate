649069926a6c80293fa4cc267ba19d4fc22b76c2
MainShayne233 <shaynetremblay@hotmail.com>
2017-04-16 03:27:31 -0400

    adds react module

--- lib/mix/tasks/app/setup/frontend/react.ex
+++ lib/mix/tasks/app/setup/frontend/react.ex
@@ -1,3 +1,14 @@
 defmodule Mix.Tasks.App.Setup.Frontend.React do
+  use Mix.Task
 
+  def run(_args \\ []) do
+    setup_assets()
+  end
+
+  def setup_assets do
+    Mix.Shell.IO.cmd("mv react_assets assets")
+    :ok
+  rescue
+    _ -> {:error, "Failed to setup react assets"}
+  end
 end
