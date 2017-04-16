b63443c4bdd9bac66a595a32d39f6cb2ea321ee8
MainShayne233 <shaynetremblay@hotmail.com>
2017-04-16 13:34:39 -0400

    adds sample elm config

--- config/setup.exs
+++ config/setup.exs
@@ -1,6 +1,15 @@
 use Mix.Config
 
+# Sample config for React frontend
+
+#config :ultimate_phoenix_boilerplate, Mix.Tasks.App.Setup,
+#  name: "NewApp",
+#  otp: "new_app",
+#  frontend: "react"
+
+# Sample config for Elm frontend
+
 config :ultimate_phoenix_boilerplate, Mix.Tasks.App.Setup,
   name: "NewApp",
   otp: "new_app",
-  frontend: "react"
+  frontend: "elm"
