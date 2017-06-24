a45e54fab67a9bfef981abc4221a4e1975980bf4
MainShayne233 <shaynetremblay@hotmail.com>
2017-06-24 02:11:13 -0400

    better field placement

--- config/setup.exs
+++ config/setup.exs
@@ -3,9 +3,9 @@ use Mix.Config
 # Sample config for React frontend
 
 config :ultimate_phoenix_boilerplate, Mix.Tasks.App.Setup,
-  ecto:     true, # set to false if you don't want to use ecto/postgres
   name:     "NewApp",
   otp:      "new_app",
+  ecto:     true,
   frontend: "react"
 
 # Sample config for Elm frontend
